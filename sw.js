678c0f8c800389eadf086341ec68a282e4bf0ce6
ashercapendala-bit <ashercapendala@gmail.com>
2026-04-14 11:46:53 +1000

    upd

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-const CACHE = "asher-tracker-v3";
+const CACHE = "asher-tracker-v4";
 const ASSETS = [
   "./",
   "./index.html",
-  "./index-1.html",
   "./manifest.json",
   "./sw.js",
   "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700;800&family=DM+Mono:wght@400;500&display=swap"
@@ -35,7 +34,7 @@ self.addEventListener("fetch", e => {
           caches.open(CACHE).then(cache => cache.put("./index.html", clone));
         }
         return res;
-      }).catch(() => caches.match("./index.html") || caches.match("./index-1.html"))
+      }).catch(() => caches.match("./index.html"))
     );
     return;
   }
@@ -48,7 +47,7 @@ self.addEventListener("fetch", e => {
         const clone = res.clone();
         caches.open(CACHE).then(cache => cache.put(e.request, clone));
         return res;
-      }).catch(() => caches.match("./index.html") || caches.match("./index-1.html"));
+      }).catch(() => caches.match("./index.html"));
     })
   );
 });
